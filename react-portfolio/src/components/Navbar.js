d04996f4dc8caed3048c8fe94d9ac874f1f40f1f
William Flores <floreswilliam891@hotmail.com>
2021-09-29 19:14:45 -0400

    adding to navbar component

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Link} from 'react-scroll'
 //React Imports from font awesome 
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
