c3915bcd2db93c55ec2e4f7206e6b6e90535ce9c
William Flores <floreswilliam891@hotmail.com>
2022-04-10 23:37:38 -0400

    fixing spelling errors

--- react-portfolio/src/components/About.js
+++ react-portfolio/src/components/About.js
@@ -14,8 +14,8 @@ const About = () => {
                     <h1 className="about-info">about me</h1>
                     <p className ="about-paragraph">
                     Developer & Creative<br></br> 
-                    <br></br>Hello, My name is William Flores. I am currently in training to become a web developer and I also recently graduated with a BA in Marketing. If you are looking someone who is efficent with developing and maintaing HTML/CSS templates, please feel free to contact me. I can also help with any marketing stratgey and creative ideas, as I have work on some marketing campagins in the past.
-                    <br></br>I currently working on my own website for my busniess. I also competencies in Microsoft Office (Word, PowerPoint, Excel), Adobe Photoshop, proficient in Google Analytics, and Social Media Marketing and have excellent Bilingual oral and written skill in both English and Spanish. I have work at Nike, Jimmy Jazz, and now a Car Dealership known as Suresky's and Sons.
+                    <br></br>Hello, My name is William Flores. I am currently in training to become a web developer and I also recently graduated with a BA in Marketing. If you are looking for someone who is efficient with developing and maintaining HTML/CSS templates, please feel free to contact me. I can also help with any marketing strategy and creative ideas, as I have worked on some marketing campaigns in the past.
+                    <br></br>I am currently working on my own website for my business. I have knowledge in Microsoft Office (Word, PowerPoint, Excel), Adobe Photoshop, proficient in Google Analytics, and Social Media Marketing and have excellent Bilingual oral and written skill in both English and Spanish. I have worked at Nike, Jimmy Jazz, and now a Car Dealership known as Suresky and Sons.
                     </p>
                 </div>
             </div>
